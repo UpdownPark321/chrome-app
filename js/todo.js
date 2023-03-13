28329a00a39d92694894749909f2bef7ac00b892
UpdownPark321 <psangh7070@gmail.com>
2023-03-13 22:28:48 +0900

    Create todo.js

---
+++ js/todo.js
@@ -0,0 +1,29 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event){
+    console.dir(event);
+}
+
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span); //li는 span이라는 자식을 갖게됨
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value; //input의 현재 value를 새 변수에 복사
+    toDoInput.value = "";
+    paintToDo(newTodo); 
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
