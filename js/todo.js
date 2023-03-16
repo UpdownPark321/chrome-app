86e18a00df3c0e49a12c466eca8364b424f10cd3
UpdownPark321 <psangh7070@gmail.com>
2023-03-16 13:44:52 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+let toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("toDos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
@@ -21,18 +23,27 @@ function paintToDo(newTodo){
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
-    li.appendChild(span); //li는 span이라는 자식을 갖게됨
+    li.appendChild(span);
     li.appendChild(button);
     toDoList.appendChild(li);
 }
 
 function handleToDoSubmit(event){
     event.preventDefault();
-    const newTodo = toDoInput.value; //input의 현재 value를 새 변수에 복사
+    const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
+    toDos.push(newTodo); //toDo를 submit할 때마다 newToDo(새로운 입력)를 push함
     paintToDo(newTodo); 
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(savedToDos!== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    console.log(parsedToDos);
+    toDos = parsedToDos;  //toDos가 빈값으로 시작하므로, 이 코드를 넣음으로써 array에 이전의 toDo들을 넣게 함.
+    parsedToDos.forEach(paintToDo); 
+}
